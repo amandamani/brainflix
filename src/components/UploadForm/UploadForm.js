b617bc087b6e0c9baa8cbf50f50345968759537d
Aman Damani <amandamani007@gmail.com>
2023-10-13 19:14:37 -0400

    Desktop CSS done

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -8,20 +8,23 @@ function UploadForm () {
             <hr className="uploadForm__topHR"/>
             <h1 className="uploadForm__heading">Upload Video</h1>
             <form>
-                <div>
-                    <h4 className="uploadForm__subHeading">VIDEO THUMBNAIL</h4>
-                    <img src={videoThumbnail} alt="thumbnail" className="uploadForm__thumbnail"/>
-                </div>
-                <div>
+                <div className="uploadForm__videoDetails">
                     <div>
-                        <h4 className="uploadForm__subHeading">TITLE YOUR VIDEO</h4>
-                        <textarea id="videoTitle" name="videoTitle" placeholder="Add a title to your video" className="uploadForm__input"></textarea>
+                        <h4 className="uploadForm__subHeading">VIDEO THUMBNAIL</h4>
+                        <img src={videoThumbnail} alt="thumbnail" className="uploadForm__thumbnail"/>
                     </div>
                     <div>
-                        <h4 className="uploadForm__subHeading">ADD A VIDEO DESCRIPTION</h4>
-                        <textarea id="videoDesc" name="videoDesc" placeholder="Add a description to your video" className="uploadForm__input uploadForm__desc"></textarea>
+                        <div>
+                            <h4 className="uploadForm__subHeading">TITLE YOUR VIDEO</h4>
+                            <textarea id="videoTitle" name="videoTitle" placeholder="Add a title to your video" className="uploadForm__input"></textarea>
+                        </div>
+                        <div>
+                            <h4 className="uploadForm__subHeading">ADD A VIDEO DESCRIPTION</h4>
+                            <textarea id="videoDesc" name="videoDesc" placeholder="Add a description to your video" className="uploadForm__input uploadForm__desc"></textarea>
+                        </div>
                     </div>
                 </div>
+                <hr className="uploadForm__desktopDisplay"/>
                 <div className="uploadForm__publish">
                     <button className="uploadForm__publish-button"><img src={publishIcon} alt="Publish Logo" className="uploadForm__publish-image"/>PUBLISH</button>
                     <h4 className="uploadForm__publish-cancel">CANCEL</h4>
